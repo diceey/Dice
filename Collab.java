373b6a8230faf63e60c226f65980ad8090d716fb
Bloxy On Github <36968271+BloxyGithub@users.noreply.github.com>
2019-02-23 15:40:34 +0000

    Update Collab.java

--- Collab.java
+++ Collab.java
@@ -1,4 +1,15 @@
-import com.teamdev.jxbrowser.chromium.Browser;
+import javax.swing.JApplet;
+import javax.swing.SwingUtilities;
+import javax.swing.JLabel;
+
+public class HelloWorld extends JApplet {
+    //Called when this applet is loaded into the browser.
+    public void init() {
+        //Execute a job on the event-dispatching thread; creating this applet's GUI.
+        try {
+            SwingUtilities.invokeAndWait(new Runnable() {
+                public void run() {
+                    import com.teamdev.jxbrowser.chromium.Browser;
 import com.teamdev.jxbrowser.chromium.JSFunctionCallback;
 import com.teamdev.jxbrowser.chromium.JSObject;
 import com.teamdev.jxbrowser.chromium.JSValue;
@@ -53,3 +64,11 @@ else if(!choice.equalsIgnoreCase("n"))
 }
 end if
     }
+
+                }
+            });
+        } catch (Exception e) {
+            System.err.println("createGUI didn't complete successfully");
+        }
+    }
+}
