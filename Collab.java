76bcc523f692af65f2c346ffaec7d7031da87f87
MasterLKG <47920824+MasterLKG@users.noreply.github.com>
2019-02-23 14:17:55 +0000

    Update Collab.java

--- Collab.java
+++ Collab.java
@@ -1,1 +1,20 @@
 import java.util.*;
+Scanner kb = new Scanner(System.in);
+boolean flag = true;
+while(flag == true)
+{
+System.out.print("Would you like to the roll the dice (Y/N)");
+String choice = kb.nextLine();
+if (choice.equalsIgnoreCase("y"))
+{
+  int dice = (int)(Math.random()*7)
+  System.out.println("The number you have rolled is " + dice);
+  System.out.println("");
+}
+else if(!choice.equalsIgnoreCase("n"))
+{
+  System.out.println("You chose to quit, despacito!");
+  flag = false;
+}
+end if
+    }
